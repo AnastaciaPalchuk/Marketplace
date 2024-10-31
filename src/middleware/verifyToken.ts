7b005c7916c2f57f3113c8fe3df2b3d4b45cd854
AnastaciaPalchuk <apalchuk16@gmail.com>
2024-10-31 15:34:53 +0200

    fixed token time

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -2,9 +2,19 @@ import jsonwebtoken from "jsonwebtoken";
 import { Context, Next } from "koa";
 
 export = (ctx: Context, next: Next) => {
-    const token = ctx.headers.authorization!;
+   try { const token = ctx.headers.authorization!;
+    if (!token) {
+        ctx.status = 401;
+        ctx.body = { message: 'Authorization token is required' };
+        return;
+    }
     const decoded = jsonwebtoken.verify(token, 'secret') as {id: string, access_type: string};
     ctx.userId = decoded.id;
     ctx.access = decoded.access_type;
     return next();
+   }
+   catch(error){
+        ctx.status = 403;
+        ctx.body = { message: 'Invalid token' };
+   }
 }
